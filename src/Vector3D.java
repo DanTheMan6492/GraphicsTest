54503d92321c397a3c8e8da4ef02827e3e3485eb
1960821 <1960821@01-19757.pusd.dom>
2022-10-19 11:03:12 -0700

    started vector class

--- src/Vector3D.java
+++ src/Vector3D.java
@@ -1,4 +1,18 @@
 
-public class Vector3D {
-
+public class Vector3D{
+	public float x, y, z;
+	
+	public Vector3D(                         ) {} //variables are initialized to 0 implicitly
+	public Vector3D(float x                  ) {this(x, x, x);}
+	public Vector3D(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public Vector3D add     (Vector3D other) {return new Vector3D(this.x + other.x,  this.y + other.y,  this.z + other.z);}	
+	public Vector3D subtract(Vector3D other) {return new Vector3D(this.x - other.x,  this.y - other.y,  this.z - other.z);}
+	public Vector3D mult    (float mult    ) {return new Vector3D(this.x * mult   ,  this.y * mult   ,  this.z*mult);     }
+	public float    dot     (Vector3D other) {return             (this.x * other.x + this.y * other.y + this.z * other.z);}
+	public Vector3D cross   (Vector3D other) {return new Vector3D(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);}
 }
